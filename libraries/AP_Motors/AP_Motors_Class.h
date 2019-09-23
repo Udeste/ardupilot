a464b2d0de8216a47875ccb208be9b27de9d60ac
Udeste <udeste@gmail.com>
2019-09-23 21:36:56 +0200

    Change MOTOR_FRAME_TYPE_K value to 99

--- libraries/AP_Motors/AP_Motors_Class.h
+++ libraries/AP_Motors/AP_Motors_Class.h
@@ -54,7 +54,7 @@ public:
         MOTOR_FRAME_TYPE_ATAIL = 5,
         MOTOR_FRAME_TYPE_Y6B = 10,
         MOTOR_FRAME_TYPE_Y6F = 11, // for FireFlyY6
-        MOTOR_FRAME_TYPE_K = 12
+        MOTOR_FRAME_TYPE_K = 99
     };
 
     // Constructor
